891e3a4839a4748ef347d6bc6dbdaba07209bd2b
Kuznetsov Dmitry <rulff@yandex.ru>
2020-10-15 01:10:58 +0300

    Setup Storyboard and the first setup of application logic

--- TrafficLightsApp/ViewController.swift
+++ TrafficLightsApp/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: IB Outlets
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var toggleTextbutton: UIButton!
+    
+    // MARK: Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+    }
+    
+    // MARK: IB Actions
+    @IBAction func clickButton() {
+        if redView.alpha < 1, yellowView.alpha < 1, greenView.alpha < 1 {
+            toggleTextbutton.setTitle("NEXT", for: .normal)
+            redView.alpha = 1
+            return
+        } else {
+            if redView.alpha == 1 {
+                redView.alpha = 0.3
+                yellowView.alpha = 1
+                greenView.alpha = 0.3
+                return
+            }
+            if yellowView.alpha == 1 {
+                redView.alpha = 0.3
+                yellowView.alpha = 0.3
+                greenView.alpha = 1
+                return
+            }
+            if greenView.alpha == 1 {
+                redView.alpha = 1
+                yellowView.alpha = 0.3
+                greenView.alpha = 0.3
+                return
+            }
+        }
+    }
 }
 
